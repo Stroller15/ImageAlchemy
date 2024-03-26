c15d806844bcdee394716a2974a27ee78cb9f02f
Stroller15 <shubhamverma6351@gmail.com>
2024-03-26 17:39:40 +0530

    feat: :sparkles: implement dynamic header for each pages
    implement dynamic header for each pages

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,21 @@
+import Header from '@/components/shared/Header'
+
+import { transformationTypes } from '@/constants';
 import React from 'react'
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = ({params: {type}}:SearchParamProps) => {
+
+  const transformation = transformationTypes[type];
+
   return (
-    <div>AddTransformationTypePage</div>
+    <>
+    <Header 
+      title={transformation.title}
+      subtitle={transformation.subTitle}
+      />
+    
+
+    </>
   )
 }
 

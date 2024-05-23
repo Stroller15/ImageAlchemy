b03afba11be557383b0c2c33bf9cdc4a0b47f256
Stroller15 <shubhamverma6351@gmail.com>
2024-05-24 00:01:24 +0530

    implement searchbar and recent edits homepage functionality
    implement searchbar with the help of cloudinary which search with the help of label as well as object

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
+import { Collection } from "@/components/shared/Collection"
 import { navLinks } from "@/constants"
+import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 
-const Home = () => {
+const Home = async ({searchParams}: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || '';
+
+  const images = await getAllImages({page, searchQuery})
+
+  
   return (
     <>
       <section className="home">
@@ -30,8 +38,17 @@ const Home = () => {
             </Link>
           ))}
         </ul>
-
       </section>
+      
+      <section className="sm:mt-12">
+          <Collection 
+            hasSearch={true}
+            images = {images?.data}
+            totalPages = {images?.totalPage}
+            page = {page}
+          />
+      </section>
+
     </>
   )
 }
--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -6,6 +6,8 @@ import { handleError } from "../utils";
 import User from "../database/models/user.model";
 import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
+import {v2 as cloudinary} from "cloudinary"
+
 
 const populateUser = (query: any) => query.populate({
     path: 'author',
@@ -91,3 +93,61 @@ export async function getImageById(imageId: string) {
     } 
 }
 
+//GET All IMAGES
+export async function getAllImages({limit = 9, page = 1, searchQuery = ''}:{
+    limit?: number,
+    page: number,
+    searchQuery?: string
+}) {
+    try {
+        await connectToDatabase();
+
+        cloudinary.config({
+            cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET,
+            secure: true,
+        })
+
+        let expression = 'folder=imagealchemy'
+
+        if(searchQuery) {
+            expression += `AND ${searchQuery}`
+        }
+
+        const {resources} = await cloudinary.search
+        .expression(expression)
+        .execute();
+
+        const resourceIds = resources.map((resource: any) => resource.public_id);
+
+        let query = {};
+
+        if(searchQuery) {
+            query = {
+                publicId: {
+                    $in: resourceIds
+                }
+            }
+        }
+
+        const skipAmount = (Number(page) -1) * limit;
+
+        const images = await populateUser(Image.find(query))
+        .sort({updateAt: -1})
+        .skip(skipAmount)
+        .limit(limit);
+
+        const totalImages = await Image.find(query).countDocuments();
+
+        const savedImages = await Image.find().countDocuments();
+
+        return {
+            data: JSON.parse(JSON.stringify(images)),
+            totalPage: Math.ceil(totalImages / limit),
+            savedImages,
+        }
+    } catch (error) {
+        handleError(error);
+    } 
+}

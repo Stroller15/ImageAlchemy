5afb1a31b794998ee6fe7c71bb441faaf485aac1
Stroller15 <shubhamverma6351@gmail.com>
2024-05-23 23:09:46 +0530

    built homePage banner with routing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,6 @@
+import { navLinks } from "@/constants"
+import Image from "next/image"
+import Link from "next/link"
 
 const Home = () => {
   return (
@@ -6,6 +9,28 @@ const Home = () => {
         <h1 className="home-heading">
           Unleash Your Creative Vision with ImageAlchemy
         </h1>
+        <ul className="flex-center w-full gap-20">
+          {navLinks.slice(1, 5).map((link) => (
+            <Link
+            key={link.route}
+            href={link.route}
+            className="flex-center flex-col gap-2"
+            >
+            <li className="flex-center w-fit rounded-full bg-white p-4">
+              <Image
+                src={link.icon}     
+                alt="image"
+                width={24}         
+                height={24}
+              />
+            </li>
+            <p className="p-4-medium text-center text-white">
+              {link.label}
+            </p>
+            </Link>
+          ))}
+        </ul>
+
       </section>
     </>
   )

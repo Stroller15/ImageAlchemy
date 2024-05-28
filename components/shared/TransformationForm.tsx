b131bbe4530767776de79b17b9db1f4e481e50f3
Stroller15 <shubhamverma6351@gmail.com>
2024-05-28 18:28:52 +0530

    fix typo

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -181,7 +181,7 @@ const TransformationForm = ({
           [fieldName === "prompt" ? "prompt" : "to"]: value,
         },
       }));
-    }, 1000);
+    }, 1000)();
     return onChangeField(value);
   };
 
